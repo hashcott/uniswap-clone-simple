76fc7081636771c3738b2dc6ab24781983944b76
Nguyen Duc Hanh <1851061469@e.tlu.edu.vn>
2024-01-08 22:03:00 +0700

    feat: get account and balance

--- src/app/component/Card.jsx
+++ src/app/component/Card.jsx
@@ -1,12 +1,56 @@
 import React from "react";
+import Image from "next/image";
 
 import styles from "../page.module.css";
 
 export const Card = (props) => {
+  const floor = (num) => {
+    // 0.9876 * 1000 = 987.6
+    // Math.floor(987.6) = 987
+    // 987 / 1000 = 0.987
+    return Math.floor(num * 1000) / 1000;
+  };
+
+  const change = (e) => {
+    // only input must have integer or float type
+    const re = /^-?\d+\.?\d*$/;
+    if (e.target.value === "" || re.test(e.target.value)) {
+      props.onChange(e.target.value);
+    }
+  };
   return (
     <div className={styles.card}>
       <h5>{props.name}</h5>
-      <input className={styles.inputNumber} type="number" placeholder="0" />
+      <input
+        disabled={props.disabled}
+        className={styles.inputNumber}
+        value={props.value}
+        onChange={change}
+        type="number"
+        min={0}
+        placeholder="0"
+      />
+      <button className={styles.buttonCoin}>
+        <Image
+          style={{ borderRadius: "100%" }}
+          width={24}
+          height={24}
+          src={`/${props.coin}.png`}
+        />
+        <span>{props.coin.toUpperCase()}</span>
+        <Image width={12} height={12} src="/arrow-down.svg" />
+      </button>
+      <span
+        style={{
+          display: props.balance ? "inline-block" : "none",
+          fontSize: "0.8rem",
+          color: "rgb(155, 155, 155)",
+          width: "100%",
+          textAlign: "right",
+        }}
+      >
+        Balance: {+props.balance >= 0 && floor(+props.balance)}
+      </span>
     </div>
   );
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,40 @@
+"use client";
+
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import Modal from "react-modal";
+import { ethers } from "ethers";
 
 import styles from "./page.module.css";
 import { Card } from "./component/Card";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [account, setAccount] = useState(false);
+  const [signer, setSigner] = useState(false);
+  const [coinFirts, setCoinFirst] = useState(undefined);
+  const [coinSeconds, setCoinSecond] = useState(undefined);
+  const [balance, setBalance] = useState("");
+
+  const connectWallet = async () => {
+    if (typeof window.ethereum === "undefined") {
+      alert("Please install MetaMask");
+      return;
+    }
+
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
+
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const balance = await provider.getBalance(accounts[0]);
+    const balanceInEth = ethers.formatEther(balance);
+    setBalance(balanceInEth);
+    await provider.send("eth_requestAccounts", []);
+    setSigner(signer);
+  };
+
   return (
     <main className={styles.main}>
       <nav className={styles.nav}>
@@ -26,14 +57,27 @@ export default function Home() {
           </li>
         </ul>
         <div className={styles.navRight}>
-          <button className={styles.button}>CONNECT</button>
+          <button onClick={() => connectWallet()} className={styles.button}>
+            {account
+              ? account.slice(0, 6) +
+                "..." +
+                account.slice(account.length - 4, account.length)
+              : "CONNECT"}
+          </button>
         </div>
       </nav>
 
       <div className={styles.center}>
         {/* write card component */}
         <div className={styles.card}>
-          <Card name="You pay" />
+          <Card
+            name="You pay"
+            coin="eth"
+            value={coinFirts}
+            onChange={setCoinFirst}
+            disabled={false}
+            balance={balance}
+          />
           {/* generate html for box  */}
           <div className={styles.wrappedBox}>
             <div className={styles.box}>
@@ -46,9 +90,22 @@ export default function Home() {
               />
             </div>
           </div>
-          <Card name="You receive" />
+          <Card
+            name="You receive"
+            coin="usdt"
+            value={coinSeconds}
+            onChange={setCoinSecond}
+            disabled={true}
+          />
+
+          <button className={styles.buttonTransfer}>Connect wallet</button>
         </div>
       </div>
+
+      {/* create model popup with react */}
+      <Modal isOpen={isOpen} contentLabel="Select a token">
+        HELLO
+      </Modal>
     </main>
   );
 }
